b63067d878982ce7c5380bd4743f9981ef3a4f0e
Sukhmanpreet Kaur <W0826062@myscc.ca>
2023-10-13 18:35:44 -0400

    Add project files.

---
+++ Data/LuminousCandle.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LuminousCandle.Models;
+
+namespace LuminousCandle.Data
+{
+    public class LuminousCandleContext : DbContext
+    {
+        public LuminousCandleContext(DbContextOptions<LuminousCandleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Candle> Candle { get; set; }
+    }
+}
---
+++ Models/Candle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace LuminousCandle.Models
+{
+    public class Candle
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime DeliveryDate { get; set; }
+        public string Wax { get; set; }
+        public string Fragrance { get; set; }
+
+
+    }
+}
