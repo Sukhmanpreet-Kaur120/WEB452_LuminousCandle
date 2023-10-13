a9c146286c95c6560bea26c5687805696d2ee7f0
Sukhmanpreet Kaur <W0826062@myscc.ca>
2023-10-13 19:27:03 -0400

    Add image - updated Index page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,6 +23,10 @@ namespace LuminousCandle.Controllers
             return View();
         }
 
+        public IActionResult About()
+        {
+            return View();
+        }
         public IActionResult Privacy()
         {
             return View();
